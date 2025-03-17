4cfadad1f28081e1180a3c7b5334b8018bb1556a
SebRodDev <sebrod35@purdue.edu>
2025-03-17 01:16:06 -0400

    fixing the read me

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 Documenting my journey through codeforces and reaching Expert rank
 
-Currently working through introductory 800 level problems
+Contests Completed:
 
-test
---
+++ string-prob.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <ios>
+
+using namespace std;
+using vi = vector<int>;
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+}
+
